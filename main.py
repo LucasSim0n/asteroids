6f25c570548de330c1bcf39957321b51eb2f437f
LucasSim0n <lucassimonfeijoo@gmail.com>
2025-05-14 23:01:14 +0200

    Asteroids has asteroids

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 
@@ -15,10 +17,16 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = updatable
 
     Player.containers = (updatable, drawable)
 
     player = Player(x, y, PLAYER_RADIUS)
+    astrofield = AsteroidField()
 
     while True:
         for event in pygame.event.get():
