a59086bff486f40f33f6446a55b289a9ea910d89
LucasSim0n <lucassimonfeijoo@gmail.com>
2025-05-14 21:16:14 +0200

    Moving triangle :D

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y, PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        dt = clock.tick(60.0) / 1000
+
         screen.fill((0, 0, 0))
+
+        player.draw(screen)
+        player.update(dt)
+
         pygame.display.flip()
 
 
